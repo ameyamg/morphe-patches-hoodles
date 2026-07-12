3f84b3c47ecf66c7cae96d17481fc9ed0be14a0a
hoo-dles <207470673+hoo-dles@users.noreply.github.com>
2026-07-12 15:13:39 -0700

    fix(SuperChinese): Allow starting pre-downloaded lessons after VIP lapse

--- patches/src/main/kotlin/hoodles/morphe/patches/superchinese/unlock/Fingerprints.kt
+++ patches/src/main/kotlin/hoodles/morphe/patches/superchinese/unlock/Fingerprints.kt
@@ -1,6 +1,7 @@
 package hoodles.morphe.patches.superchinese.unlock
 
 import app.morphe.patcher.Fingerprint
+import app.morphe.patcher.methodCall
 
 fun getGetUnlockFingerprint(className: String): Fingerprint {
     return Fingerprint(
@@ -8,3 +9,26 @@ fun getGetUnlockFingerprint(className: String): Fingerprint {
         name = "getUnlock"
     )
 }
+
+object GetVipFingerprint : Fingerprint (
+    definingClass = "Lcom/superchinese/model/User;",
+    name = "getVip"
+)
+
+object LessonStartCallbackCtorFingerprint : Fingerprint (
+    name = "<init>",
+    parameters = listOf(
+        "L",
+        "Lcom/superchinese/model/LessonStart;",
+        "Lcom/superchinese/db/bean/LessonBean;",
+        "Z",
+        "Z"
+    )
+)
+
+object ErrorFingerprint : Fingerprint (
+    name = "error",
+    filters = listOf(
+        methodCall(name = "checkNotNullParameter")
+    )
+)
--- patches/src/main/kotlin/hoodles/morphe/patches/superchinese/unlock/UnlockLessonsPatch.kt
+++ patches/src/main/kotlin/hoodles/morphe/patches/superchinese/unlock/UnlockLessonsPatch.kt
@@ -1,5 +1,7 @@
 package hoodles.morphe.patches.superchinese.unlock
 
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.extensions.InstructionExtensions.removeInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.util.returnEarly
 import hoodles.morphe.patches.superchinese.misc.signature.spoofSignaturePatch
@@ -20,6 +22,7 @@ val unlockLessonsPatch = bytecodePatch(
     dependsOn(spoofSignaturePatch)
 
     execute {
+        // Unlock all activities in UI
         val unlockClasses = mapOf(
             "Lcom/superchinese/model/BaseLesson;" to SmaliType.INTEGER,
             "Lcom/superchinese/model/LessonStart;" to SmaliType.INTEGER,
@@ -39,5 +42,26 @@ val unlockLessonsPatch = bytecodePatch(
                 }
             }
         }
+
+        // Just for UI gates after downloading
+        GetVipFingerprint.method.returnEarly(1)
+
+        // Redirect empty lesson start request callback, end(), to error() so that it uses
+        // downloaded/cached data.
+        LessonStartCallbackCtorFingerprint.classDef.apply {
+            val errorMatch = ErrorFingerprint.match(this)
+            val errorMethod = errorMatch.method
+            // remove null check since we have to pass null as message (insufficient registers)
+            errorMethod.removeInstruction(errorMatch.instructionMatches.first().index)
+
+            methods.first { it.name == "end" }.also {
+                it.addInstructions(0, """
+                    const/4 v0, 0x0
+                    invoke-virtual {p0, v0, v0}, $errorMethod
+                    return-void
+                """.trimIndent())
+            }
+        }
+
     }
 }

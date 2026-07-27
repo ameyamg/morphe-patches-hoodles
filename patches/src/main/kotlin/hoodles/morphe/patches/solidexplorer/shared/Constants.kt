2c74e23b4ffff0f25e74106146a838f43eb62d03
hoo-dles <207470673+hoo-dles@users.noreply.github.com>
2026-07-27 12:45:51 -0700

    bump solid explorer version

--- patches/src/main/kotlin/hoodles/morphe/patches/solidexplorer/shared/Constants.kt
+++ patches/src/main/kotlin/hoodles/morphe/patches/solidexplorer/shared/Constants.kt
@@ -8,6 +8,6 @@ object Constants {
         name = "Solid Explorer",
         packageName = "pl.solidexplorer2",
         appIconColor = 0x039BE5,
-        targets = listOf(AppTarget("3.4.1"))
+        targets = listOf(AppTarget("3.5.16"))
     )
 }
